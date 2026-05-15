03e5e7382a9e9372b447f87ed0a26e7c08ce02c8
Alejandro-Morato <9abril05@gmail.com>
2026-05-15 08:41:44 -0600

    subida parcial de evidencias

---
+++ Proyecto Integrador/Archivo_de_Configuracion.cpp
@@ -0,0 +1,5 @@
+[env:sparkfun_promicrorp2350]
+platform = https://github.com/maxgerhardt/platform-raspberrypi.git
+board = sparkfun_promicrorp2350
+framework = arduino
+board_build.core = earlephilhower
